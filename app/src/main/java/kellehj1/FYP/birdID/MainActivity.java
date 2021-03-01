63e8dcdf4323c6a6d36131ae79c72f677289fe61
James Kelleher <kellehj1@tcd.ie>
2021-03-01 11:52:46 +0000

    Added startup activity to create DB, & minor refactoring & restyling

--- app/src/main/java/kellehj1/FYP/birdID/MainActivity.java
+++ app/src/main/java/kellehj1/FYP/birdID/MainActivity.java
@@ -4,47 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button_fillscreen, button_create_db;
-    EditText editText;
-
-    public static final String EXTRA_MESSAGE = "kellehj1.FYP.birdID.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_fillscreen = findViewById(R.id.btn_fillscreen);
-        button_create_db = findViewById(R.id.btn_create_db);
-    }
-
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    /** Called when the user taps the Bird Time button */
-    public void birdTime(View view) {
-        Intent intent = new Intent(this, FillActivity.class);
-        startActivity(intent);
-    }
-
-    /** Called when the user taps the Bird Database button */
-    public void viewBirdList(View view) {
-        Intent intent = new Intent(this, BirdListActivity.class);
-        startActivity(intent);
-    }
-
-    public void createDB(View view) throws Exception {
         DataBaseHelper dbHelper = new DataBaseHelper(MainActivity.this,
                 "POINTED_BEAK_TABLE", "pointedbeak.json");
         if (dbHelper.getBirdsCount() > 0) {
@@ -54,8 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "DB created", Toast.LENGTH_LONG).show();
         }
         else {
-            throw new Exception("Error adding birds to database.");
+            Toast.makeText(MainActivity.this, "Error creating DB", Toast.LENGTH_LONG).show();
         }
-        //ModelTit modelTit = new ModelTit();
+
+        goToMenu();
+    }
+
+    public void goToMenu() {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/kellehj1/FYP/birdID/MainMenuActivity.java
@@ -0,0 +1,37 @@
+package kellehj1.FYP.birdID;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    Button button_fillscreen, button_create_db;
+    EditText editText;
+
+    public static final String EXTRA_MESSAGE = "kellehj1.FYP.birdID.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        button_fillscreen = findViewById(R.id.btn_fillscreen);
+    }
+
+    /** Called when the user taps the Bird Time button */
+    public void NewBirdID(View view) {
+        Intent intent = new Intent(this, FillActivity.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the Bird Database button */
+    public void viewBirdList(View view) {
+        Intent intent = new Intent(this, BirdListActivity.class);
+        startActivity(intent);
+    }
+}
